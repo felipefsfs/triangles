b4577a466945ba471a6efeec9d56b853cb84af6a
felipefsfs <Felipe>
2022-05-15 22:50:56 -0300

    AWS SDK DYNAMODB

---
+++ model/index.js
@@ -0,0 +1,50 @@
+import 'dotenv/config';
+
+import { PutItemCommand, ScanCommand  } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+
+import { ddbClient } from "./libs/ddbClient.js";
+
+const params = {
+    TableName: "triangles"
+};
+
+const scan_params = {
+    ...params,
+    FilterExpression: "timestamp_ts > :ts",
+    ExpressionAttributeValues: {
+        ":ts": { N: "0" }
+    },
+    ProjectionExpression: "timestamp_ts, lengths, result_type"
+};
+
+export {
+    get_history,
+    add
+};
+
+async function get_history() {
+    try {
+        const data = await ddbClient.send(new ScanCommand(scan_params));
+        console.log("Items:", data.Items);
+        return data.Items.map(unmarshall);
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+async function add(lengths, result_type) {
+    const add_params = {
+        ...params,
+        Item: marshall({
+            lengths,
+            result_type,
+            timestamp_ts: (new Date()).getTime()}),
+    };
+    try {
+        const data = await ddbClient.send(new PutItemCommand(add_params));
+        return data["$metadata"].httpStatusCode;
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
---
+++ model/libs/ddbDocClient.js
@@ -0,0 +1,18 @@
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { ddbClient } from "./ddbClient.js";
+
+const marshallOptions = {
+    convertEmptyValues: false,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: false
+};
+
+const unmarshallOptions = {
+    wrapNumbers: false
+};
+
+const ddbDocClient = DynamoDBDocumentClient.from(
+    ddbClient, 
+    { marshallOptions, unmarshallOptions });
+
+export { ddbDocClient };
