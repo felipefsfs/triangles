f879cd9e7bffcb07b03f66a270da18142607a525
felipefsfs <Felipe>
2022-05-16 23:29:53 -0300

    ENV PORT

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import express from "express";
 import { router } from "./routes.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT || 3000;
 
 export { app, port };
 
 app.use('/api/v1/', router);
 
 app.listen(port, () => {
-    console.log(`Listening: http://localhost:${port}/`)
+    console.log(`Listening: http://localhost:${port}/`);
 });
